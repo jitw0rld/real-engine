2deda962c769383cd9d719c8974063c8769f7072
mpxf <diamond061@outlook.com>
2022-10-04 22:36:47 -0400

    Added window/OpenGL boiler plate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+extern crate glium;
+
 fn main() {
-    println!("Hello, world!");
+    use glium::{glutin, Surface};
+
+    // Create event loop
+    let event_loop = glium::glutin::event_loop::EventLoop::new();
+
+    // Create window context
+    let wb = glium::glutin::window::WindowBuilder::new()
+        .with_title("Hello world!")
+        .with_inner_size(glium::glutin::dpi::LogicalSize::new(1024.0, 768.0));
+
+    // Create OpenGL contexts
+    let cb = glium::glutin::ContextBuilder::new();
+
+    // Create display
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
+    // Create vertex buffer/event loop
+    event_loop.run(move |ev, _, control_flow| {
+
+        // Draw frame
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.finish().unwrap();
+
+        // Frame buffer
+        let next_frame_time = std::time::Instant::now() +
+            std::time::Duration::from_nanos(16_666_667);
+        
+        // Event loop
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+        match ev {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                },
+                _ => return,
+            },
+            _ => (),
+        }
+    });
 }
