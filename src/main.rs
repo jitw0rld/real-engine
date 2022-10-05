4ef52702907082a731bca44f8c8cf42bb1d37eba
mpxf <diamond061@outlook.com>
2022-10-05 12:10:28 -0400

    Update rendering/shading engine

--- src/main.rs
+++ src/main.rs
@@ -56,9 +56,52 @@ fn main() {
             _ => (),
         }
 
-        // Draw frame
+        // Create frame in buffer
         let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
+        
+        // Initialize geometry matrix
+        let matrix = [
+            [0.5, 0.0, 0.0, 0.0],
+            [0.0, 0.5, 0.0, 0.0],
+            [0.0, 0.0, 0.5, 0.0],
+            [0.0, 0.0, 0.0, 1.0f32],
+        ];
+        
+        // Initialize perspective matrix
+        let perspective = {
+            let (width, height) = target.get_dimensions();
+            let aspect_ratio = width as f32 / height as f32;
+
+            let fov: f32 = 3.141592 / 3.0;
+            let zfar = 1024.0;
+            let znear = 0.1;
+
+            let f = 1.0 / (fov / 2.0).tan();
+
+            [
+                [f * aspect_ratio, 0.0, 0.0, 0.0],
+                [0.0, f, 0.0, 0.0],
+                [0.0, 0.0, (zfar + znear) / (znear - zfar), -1.0],
+                [0.0, 0.0, 2.0 * zfar * znear / (znear - zfar), 0.0f32],
+            ]
+        };
+
+        // Initialize lighting
+        let light = [-1.0, 0.4, 0.9f32];
+
+        // Define frame rendering parameters
+        let params = glium::DrawParameters {
+            depth: glium::Depth {
+                test: glium::DepthTest::IfLess,
+                write: true,
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        // Draw frame
+        target.draw((&positions, &normals), &indices, &program, &uniform! { matrix: matrix, perspective: perspective, light: light }, &params).unwrap();
         target.finish().unwrap();
 
     });
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,9 +1,14 @@
 #version 460
 
-in vec2 position;
+in vec3 position;
+in vec3 normal;
+
+out vec3 v_normal
 
 uniform mat4 matrix;
+uniform mat4 perspective;
 
 void main() {
-    gl_Position = matrix * vec4(position, 0.0, 1.0);
+    v_normal = transpose(inverse(mat3(matrix))) * normal;
+    gl_Position = perspective * matrix * vec4(position, 0.0, 1.0);
 }
