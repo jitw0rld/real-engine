4c3d46591f835edede05f3ad7f66b539087857b6
mpxf <diamond061@outlook.com>
2022-10-05 10:52:48 -0400

    Restructured shading elements / bug fixes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[macro_use]
 extern crate glium;
 
 fn main() {
@@ -18,12 +19,22 @@ fn main() {
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
     // Define vertex structure
+    #[derive(Copy, Clone)]
     struct Vertex {
         position: [f32; 2],
     }
 
     // Implement vertex structure
     implement_vertex!(Vertex, position);
+
+    // Initialize vertex shader
+    let vertex_shader_src = include_str!("shaders/vertex.glsl");
+
+    // Initialize fragment shader
+    let fragment_shader_src = include_str!("shaders/fragment.glsl");
+
+    // Define program
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
     // Create vertex buffer/event loop
     event_loop.run(move |ev, _, control_flow| {
@@ -36,12 +47,6 @@ fn main() {
         // Frame buffer
         let next_frame_time = std::time::Instant::now() +
             std::time::Duration::from_nanos(16_666_667);
-
-        // Initialize vertex shader
-        let vertex_shader_src = include_str!("shaders/vertex.glsl");
-
-        // Initialize fragment shader
-        let fragment_shader_src = include_str!("shaders/fragment.glsl");
         
         // Event loop
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
